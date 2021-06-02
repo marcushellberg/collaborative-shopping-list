b25443cd46ffad60654343974eb3b8bc6ba3596b
Marcus Hellberg <marcus@vaadin.com>
2021-06-02 14:34:53 -0700

    Added security config and login

--- src/main/java/com/example/application/views/shoppinglist/ShoppinglistView.java
+++ src/main/java/com/example/application/views/shoppinglist/ShoppinglistView.java
@@ -2,16 +2,16 @@ package com.example.application.views.shoppinglist;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
 @Route(value = "")
 @PageTitle("Shopping list")
 public class ShoppinglistView extends Div {
 
-    public ShoppinglistView() {
-        addClassName("shoppinglist-view");
-        add(new Text("Content placeholder"));
-    }
+  public ShoppinglistView() {
+    addClassName("shoppinglist-view");
+    add(new Text("Content placeholder"));
+  }
 
 }
