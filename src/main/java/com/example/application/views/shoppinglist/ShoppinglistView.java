6689f8b64de5b271a63438e2d2c2d28138ba0094
Marcus Hellberg <marcus@vaadin.com>
2021-06-05 14:07:01 -0700

    format code, remove extra padding

--- src/main/java/com/example/application/views/shoppinglist/ShoppinglistView.java
+++ src/main/java/com/example/application/views/shoppinglist/ShoppinglistView.java
@@ -25,128 +25,130 @@ import java.util.Map;
 @PageTitle("Shopping list")
 public class ShoppinglistView extends Div {
 
-  private final VerticalLayout shoppingList = new VerticalLayout();
-  private final ShoppingService shoppingService;
-  private final UserInfo userInfo;
-  private final Map<Integer, ItemForm> forms = new HashMap<>();
+    private final VerticalLayout shoppingList = new VerticalLayout() {{
+        setPadding(false);
+    }};
+    private final ShoppingService shoppingService;
+    private final UserInfo userInfo;
+    private final Map<Integer, ItemForm> forms = new HashMap<>();
 
-  public ShoppinglistView(ShoppingService shoppingService) {
-    addClassName("shoppinglist-view");
-    this.shoppingService = shoppingService;
+    public ShoppinglistView(ShoppingService shoppingService) {
+        addClassName("shoppinglist-view");
+        this.shoppingService = shoppingService;
 
-    var name = SecurityContextHolder.getContext().getAuthentication().getName();
-    userInfo = new UserInfo(name, name);
+        var name = SecurityContextHolder.getContext().getAuthentication().getName();
+        userInfo = new UserInfo(name, name);
 
-    var newItemForm = new ItemForm(new ShoppingListItem(), userInfo);
-    newItemForm.setSaveHandler(item -> {
-      saveItem(item);
-      newItemForm.setItem(new ShoppingListItem());
-    });
+        var newItemForm = new ItemForm(new ShoppingListItem(), userInfo);
+        newItemForm.setSaveHandler(item -> {
+            saveItem(item);
+            newItemForm.setItem(new ShoppingListItem());
+        });
 
-    var messageList = new CollaborationMessageList(userInfo, "chat");
-    var messageInput = new CollaborationMessageInput(messageList);
+        var messageList = new CollaborationMessageList(userInfo, "chat");
+        var messageInput = new CollaborationMessageInput(messageList);
 
-    add(
-        getHeader(),
-        new HorizontalLayout(
-          new VerticalLayout(newItemForm, shoppingList) {{ setPadding(false); }},
-          new VerticalLayout(messageList, messageInput)
-        )
-    );
+        add(
+            getHeader(),
+            new HorizontalLayout(
+                new VerticalLayout(newItemForm, shoppingList),
+                new VerticalLayout(messageList, messageInput)
+            )
+        );
 
-    setupCollaborationEngine(shoppingService);
-  }
+        setupCollaborationEngine(shoppingService);
+    }
 
-  Component getHeader() {
-    var header = new HorizontalLayout();
-    header.setWidthFull();
-    header.setAlignItems(FlexComponent.Alignment.BASELINE);
+    Component getHeader() {
+        var header = new HorizontalLayout();
+        header.setWidthFull();
+        header.setAlignItems(FlexComponent.Alignment.BASELINE);
 
-    var avatars = new CollaborationAvatarGroup(userInfo, "users");
-    avatars.getStyle().set("width", "unset"); // Why does not setSizeUndefined work?
-    var h1 = new H1("Shopping list");
-    header.add(h1, avatars);
-    header.expand(h1);
-    return header;
-  }
+        var avatars = new CollaborationAvatarGroup(userInfo, "users");
+        avatars.getStyle().set("width", "unset"); // Why does not setSizeUndefined work?
+        var h1 = new H1("Shopping list");
+        header.add(h1, avatars);
+        header.expand(h1);
+        return header;
+    }
 
-  private void setupCollaborationEngine(ShoppingService shoppingService) {
-    CollaborationEngine.getInstance().openTopicConnection(this, "list", userInfo, topicConnection -> {
-      var items = topicConnection.getNamedMap("items");
+    private void setupCollaborationEngine(ShoppingService shoppingService) {
+        CollaborationEngine.getInstance().openTopicConnection(this, "list", userInfo, topicConnection -> {
+            var items = topicConnection.getNamedMap("items");
 
-      items.subscribe(e -> {
-        if(e.getOldValue(ShoppingListItem.class) == null) { // new
-          addItem(e.getValue(ShoppingListItem.class));
-        } else {
-          if(e.getValue(ShoppingListItem.class) == null) { // deleted
-            deleteItem(e.getOldValue(ShoppingListItem.class));
-          } else { // updated
-            var updated = e.getValue(ShoppingListItem.class);
-            forms.get(updated.getId()).setItem(updated);
-          }
+            items.subscribe(e -> {
+                if (e.getOldValue(ShoppingListItem.class) == null) { // new
+                    addItem(e.getValue(ShoppingListItem.class));
+                } else {
+                    if (e.getValue(ShoppingListItem.class) == null) { // deleted
+                        deleteItem(e.getOldValue(ShoppingListItem.class));
+                    } else { // updated
+                        var updated = e.getValue(ShoppingListItem.class);
+                        forms.get(updated.getId()).setItem(updated);
+                    }
+                }
+            });
+
+            // Init the map if it is empty
+            if (items.getKeys().count() == 0) {
+                shoppingService.getShoppingList().forEach(this::addItem);
+            }
+
+            return null;
+        });
+    }
+
+    void addItem(ShoppingListItem item) {
+        var form = new ItemForm(item, userInfo);
+        form.setSaveHandler(this::saveItem);
+
+        if (item.getId() != null) { // Only for saved items
+            form.setDeleteHandler(this::deleteItem);
         }
-      });
-
-      // Init the map if it is empty
-      if(items.getKeys().count() == 0) {
-        shoppingService.getShoppingList().forEach(this::addItem);
-      }
-
-      return null;
-    });
-  }
-
-  void addItem(ShoppingListItem item) {
-    var form = new ItemForm(item, userInfo);
-    form.setSaveHandler(this::saveItem);
-
-    if(item.getId() != null) { // Only for saved items
-      form.setDeleteHandler(this::deleteItem);
+        forms.put(item.getId(), form);
+        shoppingList.add(form);
     }
-    forms.put(item.getId(), form);
-    shoppingList.add(form);
-  }
 
-  void saveItem(ShoppingListItem updated){
-    try {
-      var saved = shoppingService.saveItem(updated);
+    void saveItem(ShoppingListItem updated) {
+        try {
+            var saved = shoppingService.saveItem(updated);
 
-      CollaborationEngine.getInstance().openTopicConnection(this, "list", userInfo, topicConnection -> {
-        var items = topicConnection.getNamedMap("items");
-        items.put(saved.getId().toString(), saved);
+            CollaborationEngine.getInstance().openTopicConnection(this, "list", userInfo, topicConnection -> {
+                var items = topicConnection.getNamedMap("items");
+                items.put(saved.getId().toString(), saved);
 
-        return null;
-      });
+                return null;
+            });
 
-    } catch (ObjectOptimisticLockingFailureException e){
-      showSaveError();
+        } catch (ObjectOptimisticLockingFailureException e) {
+            showSaveError();
+        }
     }
-  }
 
-  void deleteItem(ShoppingListItem item){
-    try {
-      shoppingService.deleteItem(item);
-      shoppingList.remove(forms.get(item.getId()));
-      forms.remove(item.getId());
+    void deleteItem(ShoppingListItem item) {
+        try {
+            shoppingService.deleteItem(item);
+            shoppingList.remove(forms.get(item.getId()));
+            forms.remove(item.getId());
 
-      // Update the shared
-      CollaborationEngine.getInstance().openTopicConnection(this, "list", userInfo, topicConnection -> {
-        var items = topicConnection.getNamedMap("items");
-        items.put(item.getId().toString(), null); // no delete API?
+            // Update the shared
+            CollaborationEngine.getInstance().openTopicConnection(this, "list", userInfo, topicConnection -> {
+                var items = topicConnection.getNamedMap("items");
+                items.put(item.getId().toString(), null); // no delete API?
 
-        return null;
-      });
+                return null;
+            });
 
-    } catch (ObjectOptimisticLockingFailureException e){
-      showSaveError();
+        } catch (ObjectOptimisticLockingFailureException e) {
+            showSaveError();
+        }
     }
-  }
 
-  private void showSaveError() {
-    var notification = new Notification("Save conflict. Please try again.");
-    notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
-    notification.setPosition(Notification.Position.MIDDLE);
-    notification.open();
-  }
+    private void showSaveError() {
+        var notification = new Notification("Save conflict. Please try again.");
+        notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+        notification.setPosition(Notification.Position.MIDDLE);
+        notification.open();
+    }
 
 }
